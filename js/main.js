e638dcd1261c2934568add6b9fbfa87292d3cb43
Anderson F. Silva <www.anderson-ferdnand07@hotmail.com>
2022-11-17 16:27:28 -0300

    exercicio concluido

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+$('.telefone').mask('(00) 00000-0000');
+$('.dataNascimento').mask('00/00/0000');
+$('.rg').mask('00.000.000-0');
+$('.cpf').mask('000.000.000-00');
+$('.cep').mask('00000-000');
+});
