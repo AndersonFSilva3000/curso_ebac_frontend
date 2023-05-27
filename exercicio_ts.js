498ae9d8978ffd6522e0e4d101b6e87fc85da764
Anderson F. Silva <www.anderson-ferdnand07@hotmail.com>
2023-05-27 03:15:45 -0300

    Exercicio TypeScript

---
+++ exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function calcular(n1, n2) {
+    return n1 * n2;
+}
+function saudacao(nome) {
+    return `Olá, eu sou o ${nome}. :D`;
+}
+console.log(calcular(5, 5));
+console.log(saudacao("Anderson"));
---
+++ exercicio_ts.ts
@@ -0,0 +1,10 @@
+function calcular(n1: number, n2: number): number {
+  return n1 * n2;
+}
+
+function saudacao(nome: string): string {
+  return `Olá, eu sou o ${nome}. :D`
+}
+
+console.log(calcular(5, 5));
+console.log(saudacao("Anderson"));
